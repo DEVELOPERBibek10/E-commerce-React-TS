19c23b883169676a8e377922c3ce3d2c97c2b23b
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-10-11 16:15:28 +0545

    add minor update in protected layout component

--- src/Components/Layout/ProtectedLayout.tsx
+++ src/Components/Layout/ProtectedLayout.tsx
@@ -5,6 +5,7 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const ProtectedLayout = () => {
   const { user, loading } = useSelector((state: RootState) => state.Auth);
+  const total = useSelector((state: RootState) => state.Cart.cartBill.total);
   const location = useLocation();
 
   if (loading) {
@@ -13,6 +14,8 @@ const ProtectedLayout = () => {
 
   if (!user) {
     return <Navigate to="/login" replace state={{ from: location }} />;
+  } else if (!Number(total)) {
+    return <Navigate to="/" replace state={{ from: location }} />;
   }
 
   return <Outlet />;
